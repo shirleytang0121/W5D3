bb9a5a851f1d14ad5267ce7c066ec58551932319
0skat <81402903+0skat@users.noreply.github.com>
2021-06-09 13:34:12 -0700

    Finished import_db

--- import_db.sql
+++ import_db.sql
@@ -14,7 +14,7 @@ CREATE TABLE questions(
     id INTEGER PRIMARY KEY,
     title TEXT NOT NULL,
     body TEXT NOT NULL,
-    a_author_id INTEGER,
+    a_author_id INTEGER NOT NULL,
     FOREIGN KEY (a_author_id) REFERENCES users(id)
 );
 
@@ -22,8 +22,8 @@ DROP TABLE IF EXISTS question_follows;
 
 CREATE TABLE question_follows(
     id INTEGER PRIMARY KEY,
-    user_id INTEGER,
-    question_id INTEGER,
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (question_id) REFERENCES questions(id)
 );
@@ -32,8 +32,8 @@ DROP TABLE IF EXISTS replies;
 
 CREATE TABLE replies(
     id INTEGER PRIMARY KEY,
-    question_id INTEGER,
-    user_id INTEGER,
+    question_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
     parent_id INTEGER,
     reply_body TEXT NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id),
@@ -45,9 +45,40 @@ DROP TABLE IF EXISTS question_likes;
 
 CREATE TABLE question_likes(
     id INTEGER PRIMARY KEY,
-    likes INTEGER,
-    question_id INTEGER,
-    user_id INTEGER,
+    question_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (question_id) REFERENCES questions(id)
 );
+
+INSERT INTO 
+  users(fname,lname)
+VALUES 
+  ('Shirley','Tang'),
+  ('Oscar', 'Vazquez'); 
+
+INSERT INTO 
+  questions(title, body, a_author_id)
+VALUES
+  ('confused','How do I do this project?', (SELECT id FROM users WHERE fname = 'Shirley')),
+  ('lunch', 'Whats for lunch?', (SELECT id FROM users WHERE fname = 'Oscar')); 
+
+  INSERT INTO 
+    question_follows(user_id,question_id)
+  VALUES
+   ((SELECT id FROM users WHERE fname = 'Shirley'), (SELECT id FROM questions WHERE title = 'confused')),
+   ((SELECT id FROM users WHERE fname = 'Oscar'), (SELECT id FROM questions WHERE title = 'lunch'));
+
+  INSERT INTO 
+    replies(question_id,user_id,parent_id,reply_body)
+  VALUES 
+    ((SELECT id FROM questions WHERE title = 'confused'), (SELECT id FROM users WHERE fname = 'Oscar'), NULL, 'Im just as confused'),
+    ((SELECT id FROM questions WHERE title = 'confused'), (SELECT id FROM users WHERE fname = 'Shirley'),
+    (SELECT id FROM replies WHERE reply_body = 'Im just as confused'), 'Something else' );
+
+
+INSERT INTO 
+  question_likes(question_id, user_id)
+VALUES 
+  ((SELECT id FROM questions WHERE title = 'lunch'), (SELECT id FROM users WHERE fname = 'Shirley')),
+  ((SELECT id FROM questions WHERE title = 'lunch'), (SELECT id FROM users WHERE fname = 'Oscar'));
